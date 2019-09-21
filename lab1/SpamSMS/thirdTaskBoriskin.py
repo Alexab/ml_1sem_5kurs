cf28aa76ed1de545c9026a30a45b1b80728dfe4b
Makary Boriskin <makary.b@gmail.com>
2019-09-21 14:02:42 +0300

    Добавляет третью часть третьего задания

--- lab1/SpamSMS/thirdTaskBoriskin.py
+++ lab1/SpamSMS/thirdTaskBoriskin.py
@@ -69,15 +69,15 @@ print("Средняя длина содержимого всех файлов:",
 
 #вторая часть третьего задания:
 
-def charactersInFiles(fileName, folder, characters):
+def charactersInFiles(fileName, characters):
 	for key in characters:
 		characters[key] = 0
 	f = open(fileName, "r", encoding='utf-8')
 	for line in f:
 		for c in line:
-				for key in characters:
-					if c == key:
-						characters[c] += 1
+			for key in characters:
+				if c == key:
+					characters[c] += 1
 	f.close()
 	return characters
 
@@ -95,7 +95,7 @@ def charactersCounter(folder, N):
 	characters_final = madeDictionary()
 	for i in range (N):
 		fileName = folder + str(i+1) + ".txt"
-		characters_temp = charactersInFiles(fileName,folder,characters)
+		characters_temp = charactersInFiles(fileName,characters)
 		for key in characters_temp:
 			characters_final[key] += characters_temp[key]
 	return characters_final
@@ -104,11 +104,11 @@ characters_ham = charactersCounter("ham/", ham)
 characters_spam = charactersCounter("spam/", spam)
 
 print("\nДля папки ham:\n")
-for key, value in characters_ham.items():
-	print(key,value)
+for i in sorted(characters_ham.items(), key = lambda para: para[1]):
+	print(i)
 print("\nДля папки spam:\n")
-for key, value in characters_spam.items():
-	print(key,value)
+for i in sorted(characters_spam.items(), key = lambda para: para[1]):
+	print(i)
 
 print("\nВ общем:\n")
 
@@ -119,12 +119,76 @@ for key in characters_ham:
 for key in characters_spam:
 	characters[key] += characters_spam[key]
 
-for key, value in characters.items():
-	print(key,value)
+for i in sorted(characters.items(), key = lambda para: para[1]):
+	print(i)
 
 
 #третья часть третьего задания
 
+def wordsInFiles(fileName, words):
+	lst = []
+	for key in words:
+		words[key] = 0
+	file = open(fileName, "r", encoding='utf-8')
+	for line in file:
+		lst = line.lower().replace('.','').split()
+		for i in lst:
+			for key in words:
+				if key == i:
+					words[key] += 1
+	file.close()
+	return words
 
+def madeWordsDictionary():
+	words = {}
+	file = open("SMSSpamCollection.txt", "r", encoding='utf-8')
+	for line in file:
+		lst = line.lower().replace('.','').split()
+		for word in lst:
+			words[word] = 0
+	file.close()
+	return words
 
+def wordsCounter(folder, N):
+	words = madeWordsDictionary()
+	words_final = madeWordsDictionary()
+	for i in range (N):
+		fileName = folder + str(i+1) + ".txt"
+		words_temp = wordsInFiles(fileName,words)
+		for key in words_temp:
+			words_final[key] += words_temp[key]
+	return words_final
 
+def dictSize(words):
+	size = 0
+	for key in words:
+		size += 1
+	return size
+
+words_ham = wordsCounter("ham/", ham)
+words_spam = wordsCounter("spam/", spam)
+
+size = dictSize(words_ham)
+
+print("\nДля папки ham:\n")
+for i in sorted(words_ham.items(), key = lambda para: para[1]):
+	if i[1] > 500:
+		print (i)
+
+print("\nДля папки spam:\n")
+for i in sorted(words_spam.items(), key = lambda para: para[1]):
+	if i[1] > 100:
+		print (i)
+
+print("\nВ общем:\n")
+
+words = madeWordsDictionary()
+
+for key in words_ham:
+	words[key] += words_ham[key]
+for key in words_spam:
+	words[key] += words_spam[key]
+
+for i in sorted(words.items(), key = lambda para: para[1]):
+	if i[1] > 200:
+		print (i)
