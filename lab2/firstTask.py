878c21cc8daf57677234a2cdfb96d33353d45ead
Makary <makary.b@gmail.com>
2019-11-01 20:38:39 +0300

    Добавляет решение первой части второй лаб работы без пунктов 4 и 6

---
+++ lab2/firstTask.py
@@ -0,0 +1,127 @@
+# created by БорискинМА
+# 01.11.19
+# gedit
+import numpy as np
+import pandas as pd
+import pylab as pl
+import seaborn as sns
+import graphviz
+import folium
+from sklearn import neighbors, datasets 
+from matplotlib import pyplot as plt
+from matplotlib import style
+from folium import plugins
+from folium.plugins import HeatMap
+from itertools import islice
+
+data = pd.read_csv('AB_NYC_2019.csv')
+
+def totalABNYC2019():
+	print('Количество записей в базе:', len(set(data['id'])), '\n')
+
+def histogram():
+
+	data.info()
+	print('\n')	
+
+	data.hist(figsize=(15,8), layout=(2,5))
+
+	plt.show()
+
+def areThereAnyMissingItems():
+	print(data.isnull().sum())
+	print('\n')
+	print ('Broken items in', 'столбцы \"дата последнего отзыва\" и \"количество отзывов в месяц\"', 'are the same?', (data['last_review'].isnull() == data['reviews_per_month'].isnull()).all())
+	print('\n')
+
+def fillMissingItems():
+	data.loc[data['reviews_per_month'].isnull(), 'reviews_per_month'] = 0
+	
+def checkNumericVariables():
+	print(data.describe())
+	print('\n')
+
+def removeUnnecessaryColumns():
+	data.drop(['id','name','host_name','last_review'], axis = 1, inplace = True)
+
+def cleanAbnormalities():
+	dataNYC = data[data['price'] > 0]
+	dataNYC = data[data['minimum_nights'] < 366]
+	dataNYC.reset_index(drop = True, inplace = True)
+	dataNYC.info()
+	print('\n')
+	return dataNYC
+
+#def latlonNbrhdGr():
+	
+	#data.plot(x = 'latitude', y = 'longitude', )	
+
+
+	#sns.distplot(data['latitude'], kde=False)	
+
+	#plt.ylabel('longitude')
+
+	#plt.show()
+
+def heatMap():
+	m = folium.Map([40.7128, -74.0060], zoom_start = 11)
+	data_loc = data[['latitude','longitude']].values
+	data_loc = data_loc.tolist()
+	HeatMap(data[['latitude','longitude']].dropna(), radius = 8, gradient = {0.2:'blue', 0.4:'purple', 0.6:'orange', 1.0:'red'}).add_to(m)
+
+	#if you want you can open it manually in your webbrowser
+	m.save("heatMap.html")
+
+def correlation():
+	print(set(data['neighbourhood_group']))
+	print('\n')
+
+	for group in set(data['neighbourhood_group']):
+		print(group)
+		print(data[data['neighbourhood_group'] == group][['price','minimum_nights', 'availability_365']].corr())
+		print('\n')
+	sns.distplot(data['minimum_nights'])
+	plt.show()
+
+	corr = data[data['neighbourhood_group'] == group][['price','minimum_nights', 'availability_365']].corr()
+	sns.heatmap(corr, xticklabels = corr.columns.values, yticklabels = corr.columns.values)
+	plt.title('Correlation Color Matrix')
+	plt.show()
+
+	print('\n')
+
+def wordsCounter():
+	name = pd.read_csv('AB_NYC_2019.csv', usecols = [1], squeeze = True)	
+
+	dictionary = {}
+	dictionary = name.str.lower().str.split(None, expand = True).stack().value_counts()	
+
+	print('25 наиболее часто встречаемых слов в колонке name:\n')
+	print(list(islice(dictionary.items(), 25)))
+	print('\n')
+
+#def latlonPrice():
+
+
+#1
+totalABNYC2019()
+#2
+histogram()
+#3
+areThereAnyMissingItems()
+fillMissingItems()
+checkNumericVariables()
+removeUnnecessaryColumns()
+data = cleanAbnormalities()
+#4
+#latlonNbrhdGr()
+heatMap()
+#5
+correlation()
+#6
+#latlonPrice()
+#7
+wordsCounter()
+
+
+
